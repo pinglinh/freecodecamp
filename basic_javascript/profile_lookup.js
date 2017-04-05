39117b8acc66d2da6a8a9083e7fc61adb126528e
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-04-05 18:34:57 +0100

    Create profile_lookup.js

---
+++ basic_javascript/profile_lookup.js
@@ -0,0 +1,70 @@
+
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(firstName, prop) {
+
+  for (i = 0; i < contacts.length; i++) {
+    var contact = contacts[i];
+    var contactFirstName = contact.firstName;
+    var thisIsCorrectContact = (contactFirstName == firstName);
+    var contactHasProp = contact.hasOwnProperty(prop);
+    var contactPropValue = contact[prop];
+    
+    if (thisIsCorrectContact) {
+      if (contactHasProp) {
+        return contactPropValue;
+      }
+     return "No such property";
+    }
+  }
+  return "No such contact";
+  
+}
+
+lookUpProfile("Akira", "likes");
+
+
+
+
+
+
+// Alternative code:
+function lookUpProfile(firstName, prop) {
+
+  for (var i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName == firstName) {
+      if(contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } 
+      return "No such property";
+    } 
+  }
+  return "No such contact";
+}
