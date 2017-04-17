541803d6232259c37be877458cdef8c80b4813c9
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-04-17 12:17:43 +0100

    Added solution to confirm the ending challenge

--- basic_algorithm_scripting/confirm_the_ending.js
+++ basic_algorithm_scripting/confirm_the_ending.js
@@ -1,6 +1,18 @@
 function confirmEnding(str, target) {
+var checkString = str.substr(-target.length, target.length);
 
-  return str;
+  if (checkString == target) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
 }
 
-confirmEnding("Bastian", "n");
+confirmEnding("Bastian", "n"); // should return true
+confirmEnding("He has to give me a new name", "name") // should return true
+confirmEnding("Open sesame", "same") // should return true
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") // should return false
+
+// At first I did str.substr(-1, target.length) however -1 only selects from last letter so for other tests when the last letter of str and target were same, it would return true when it's false because words did not match. To fix this I did -target.length which would select the last word. The substr() arguments can also be put into variables if desired.
