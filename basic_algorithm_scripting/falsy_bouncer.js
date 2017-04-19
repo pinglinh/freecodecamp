541df4a129ef81de6e034671b429c1186da07872
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-04-19 20:06:30 +0100

    Added extra solution to falsey bouncer

--- basic_algorithm_scripting/falsy_bouncer.js
+++ basic_algorithm_scripting/falsy_bouncer.js
@@ -1,5 +1,4 @@
-// When you return from filter - it converts the value to true/false and drops the values where false is returned
-Since you wanted to drop the falses it works (as if by magic)
+// When you return from filter - it converts the value to true/false and drops the values where false is returned.
 
 function bouncer(arr) {
   var nonFalsey = arr.filter(function(val) {
@@ -10,3 +9,60 @@ function bouncer(arr) {
 
 bouncer([1, null, NaN, 2, undefined]);
 
+
+// Shorter version
+
+function bouncer(arr) {
+  return arr.filter(function(val) {
+    return val;
+  });
+}
+
+bouncer([1, null, NaN, 2, undefined]);
+
+
+
+
+// Creating separate functions for all the falsey values to use inside the filter function. We can then do ! to return us values which are not false, null, 0, "", undefined and NaN.
+
+// NaN is not-a-number, numbers such as 1,2,3,4 etc. are not NaN therefore will still return in the final array.
+
+
+
+function isFalse(val) {
+  return val === false;
+}
+
+function isNull(val) {
+  return val === null;
+}
+
+function isZero(val) {
+  return val === 0;
+}
+
+function isEmptyString(val) {
+  return val === "";
+}
+
+function isUndefined(val) {
+  return val === undefined;
+}
+
+
+
+function bouncer(arr) {
+  var nonFalsey = arr.filter(function(val) {
+    return !(isFalse(val) ||
+            isNull(val) ||
+            isZero(val) ||
+            isEmptyString(val)||
+            isUndefined(val) ||
+            isNaN(val));
+  });
+ return nonFalsey;
+}
+
+bouncer([1, null, NaN, 2, undefined]);
+bouncer([false, null, 0, NaN, undefined, ""])
+bouncer([1, null, NaN, 2, undefined])
