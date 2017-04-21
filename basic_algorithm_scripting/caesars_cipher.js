0d7c1882f67bd9f975d3b8f47b9a41718871e96a
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-04-21 13:00:59 +0100

    Updated caesars cipher challenge

--- basic_algorithm_scripting/caesars_cipher.js
+++ basic_algorithm_scripting/caesars_cipher.js
@@ -17,17 +17,61 @@
 // ["S", "E", "R", "R", "  ", "P", "B", "Q", "R", " "," P", "N", "Z", "C"]
 
 
-function capitalize(val) {
-  if (val == val.toUpperCase()) {
-    return true;
+
+
+function rotate(val) {
+  if (val !== val.toLowerCase()) {
+    return "X";
   }
-  else if (val !== val.toLowerCase()) {
-    return val;
-  }
-  
   else {
-    return false;
+    return val;
   }
 }
 
-capitalize(".")
+[rotate("A"), rotate("."), rotate ("c"]);
+
+
+
+
+
+function rot13(str) {
+  var result = "";
+  var splitStr = str.split("");
+  var decoded = {
+    "A": "N",
+    "B": "O",
+    "C": "P",
+    "D": "Q",
+    "E": "R",
+    "F": "S",
+    "G": "T",
+    "H": "U",
+    "I": "V",
+    "J": "W",
+    "K": "X",
+    "L": "Y",
+    "M": "Z",
+    "N": "A",
+    "O": "B",
+    "P": "C",
+    "Q": "D",
+    "R": "E",
+    "S": "F",
+    "T": "G",
+    "U": "H",
+    "V": "I",
+    "W": "J",
+    "X": "K",
+    "Y": "L",
+    "Z": "M"
+  }
+  
+  for (var i = 0; i < splitStr.length; i++) {
+    result = decoded[splitStr];
+  }
+  
+  
+  return result;
+}
+
+rot13("SERR PBQR PNZC");
