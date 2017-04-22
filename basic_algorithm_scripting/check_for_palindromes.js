23fee9187bb27140f1f5c914801f739c863a53fb
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-04-22 17:32:36 +0100

    Added extra solution to check palindrome challenge

--- basic_algorithm_scripting/check_for_palindromes.js
+++ basic_algorithm_scripting/check_for_palindromes.js
@@ -1,18 +1,30 @@
 function palindrome(str) {
   var editedString = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-  
+
   var reversedEditedString = editedString.split("").reverse().join("");
-  
-  if (editedString == reversedEditedString) { 
+
+  if (editedString == reversedEditedString) {
     return true;
   }
-  
+
   else {
     return false;
   }
 }
 
 palindrome("_eye");
+
+
+// Shorter solution
+
+function palindrome(str) {
+  editedString = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  return reversedEditedString === editedString.split("").reverse().join("");
+}
+
+palindrome("_eye");
+
+
 
 // The below lists all the punctuation and symbols on the keyboard, some of which had to be escaped with a \.
 // This is not as good of an approach as there are many more symbols in unicode as well as letters which may have accents on them
@@ -21,13 +33,13 @@ palindrome("_eye");
 
 function palindrome(str) {
   var editedString = str.replace(/\s+|[-_!"£$€¦%&*(\)+\=\-`¬[\]{}:;@'~#<>?,.\^\/\\|]/g, "").toLowerCase();
-  
+
   var reversedEditedString = editedString.split("").reverse().join("");
-  
-  if (editedString == reversedEditedString) { 
+
+  if (editedString == reversedEditedString) {
     return true;
   }
-  
+
   else {
     return false;
   }
