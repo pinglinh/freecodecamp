dd9a168e0221156384141b73be49d0a41d13498a
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-04-22 18:21:11 +0100

    Added alternative solution to return largest numbers in arrays challenge

--- basic_algorithm_scripting/return_largest_numbers_in_arrays.js
+++ basic_algorithm_scripting/return_largest_numbers_in_arrays.js
@@ -13,3 +13,16 @@ function largestOfFour(arr) {
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+// Alternative solution
+
+function largestOfFour(arr) {
+  return arr.map(function(insideArray) {
+    return insideArray.reduce(function(x, y) {
+      return Math.max(x, y); // This compares the highest number against all the other numbers in the array. Once a higher number is found, it is then compared against the following numbers until the last. e.g. 4 vs 4, 4 vs 5, 5 vs 1, 5 vs 1 etc.
+    }, insideArray[0]); // // We are starting the comparison with the first element in the array
+  });
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
