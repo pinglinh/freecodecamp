f0e4cbd9a003bfc95432348a53f729f00f404ed8
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-04-22 19:03:44 +0100

    Added alternative solution to slasher flick challenge

--- basic_algorithm_scripting/slasher_flick.js
+++ basic_algorithm_scripting/slasher_flick.js
@@ -6,3 +6,12 @@ function slasher(arr, howMany) {
 slasher([1, 2, 3], 2);
 
 // splice() method modifies the array therefore we can return arr
+
+
+// Simple solution
+
+function slasher(arr, howMany) {
+  return arr.slice(howMany);
+}
+
+slasher([1, 2, 3], 2);
