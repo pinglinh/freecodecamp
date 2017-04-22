fbeb55605f41ce9dc61693aed03486f9b46bf294
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-04-22 23:40:16 +0100

    Added alternative solutions to caesars cipher challenge

--- basic_algorithm_scripting/caesars_cipher.js
+++ basic_algorithm_scripting/caesars_cipher.js
@@ -1,21 +1,3 @@
-// function rot13(str) { // LBH QVQ VG!
-//   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   var decoded = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-//   var splitStr = str.split("");
-//   for (var i = 0; i < splitStr.length; i++) {
-//     if (splitStr 
-//   }
-//   return str;
-// }
-
-// // Change the inputs below to test
-// rot13("SERR PBQR PNZC");
-
-
-// SERR PBQR PNZC
-
-// ["S", "E", "R", "R", "  ", "P", "B", "Q", "R", " "," P", "N", "Z", "C"]
-
 function rot13(str) {
   var result = "";
   var splitStr = str.split("");
@@ -50,10 +32,10 @@ function rot13(str) {
   };
   for (var i = 0; i < splitStr.length; i++) {
     var splitStrIndex = splitStr[i];
-    var capitalLetters = /[A-Z]/g; 
+    var capitalLetters = /[A-Z]/g;
     if (capitalLetters.test(splitStrIndex)) {
       result += decoded[splitStrIndex];
-    } 
+    }
     else {
       result += splitStrIndex;
     }
@@ -62,3 +44,55 @@ function rot13(str) {
 }
 
 rot13("SERR PBQR PNZC!");
+
+
+
+// Alternative solution
+
+
+
+function rot13(str) {
+  var result = "";
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var decoded = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+  for (var character of str) {
+    var index = alphabet.indexOf(character);
+    if (index === -1) {
+      result += character;
+    }
+    else {
+      result += decoded[index];
+    }
+  }
+  return result;
+}
+
+rot13("SERR PBQR PNZC!");
+
+
+
+// Solution using charCodeAt() and fromCharCode()
+
+
+
+function rot13(str) {
+  var result = "";
+  for (var character of str) {
+    var index = character.charCodeAt(0);
+    var A_index = "A".charcodeAt(0);
+    var M_index = "M".charcodeAt(0);
+    var N_index = "N".charcodeAt(0);
+    var Z_index = "Z".charcodeAt(0);
+    if (index >= A_index && index <= M_index) {
+      index += 13;
+    }
+    else if (index >= N_index && index <= Z_index) {
+      index -= 13;
+    }
+    result += String.fromCharCode(index);
+  }
+  return result;
+}
+
+rot13("SERR PBQR PNZC!");
+
