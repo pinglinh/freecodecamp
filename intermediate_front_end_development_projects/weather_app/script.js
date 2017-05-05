3a67d237fa331027ccd48a273562515a819afff4
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-05-06 00:11:51 +0100

    Mapped weather API icons to weather font icons

--- intermediate_front_end_development_projects/weather_app/script.js
+++ intermediate_front_end_development_projects/weather_app/script.js
@@ -1,11 +1,40 @@
 function set_weather(data) {
   $(".location").text(
-    // JSON.stringify()
-    "" + data.main.temp + " C"
-   );
-  var icon = data.weather[0].icon;
-  $(".weather-icon").attr("src", "http://openweathermap.org/img/w/"+icon+".png"
+    data.name + ", " + data.sys.country
   );
+
+  $(".temp").text(
+    // JSON.stringify() // gets all the json
+    "" + Math.floor(data.main.temp) + " °C"
+  );
+
+  var icon_to_font = {
+  "01d": "wi-day-sunny",
+  "01n": "wi-night-clear",
+  "02d": "wi-day-cloudy",
+  "02n": "wi-night-alt-cloudy",
+  "03d": "wi-cloud",
+  "03n": "wi-cloud",
+  "04d": "wi-cloudy",
+  "04n": "wi-cloudy",
+  "09d": "wi-day-showers",
+  "09n": "wi-night-alt-showers",
+  "10d": "wi-day-rain",
+  "10n": "wi-night-alt-rain",
+  "11d": "wi-day-thunderstorm",
+  "11n": "wi-night-alt-thunderstorm",
+  "13d": "wi-day-snow",
+  "13n": "wi-night-alt-snow",
+  "50d": "wi-windy",
+  "50n": "wi-windy",
+  }
+
+  var icon = data.weather[0].icon;
+  var font_icon = icon_to_font[icon];
+
+  // $(".weather-icon").attr("src", "http://openweathermap.org/img/w/"+icon+".png"
+  // );
+  $(".wi").attr("class", "wi " + font_icon);
 }
 
 $(document).ready(function() {
