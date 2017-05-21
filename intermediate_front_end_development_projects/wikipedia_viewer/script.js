d15d8768604a986a87f4172e56123de5d0e3e35b
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-05-21 22:48:17 +0100

    Added ajax call for search function

--- intermediate_front_end_development_projects/wikipedia_viewer/script.js
+++ intermediate_front_end_development_projects/wikipedia_viewer/script.js
@@ -6,6 +6,24 @@ function search_results(data) {
     $(".results-list").append(li);
     var link = "https://en.wikipedia.org/wiki/" +
     r.title;
-    li.html()
+    li.html("<h3><a href='"+link+"' target='_new_'>"+r.title+"</a></h3>"+"<div class='snippet'>"+r.snippet+"</div>"
+      );
   }
+}
+
+function search() {
+  var query = $('#search').val();
+  console.log("searching", query);
+  $.ajax({
+    url: 'https://en.wikipedia.org/w/api.php',
+    data: {
+      action: 'query',
+      list: 'search',
+      srsearch: query,
+      format: 'json',
+      srprop: 'snippet'
+    },
+    dataType: 'jsonp',
+    success: search_results
+  });
 }
