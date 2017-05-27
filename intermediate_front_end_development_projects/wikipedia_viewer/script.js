d17b253f500784bb26cd1cfcc0c7ca838b9a8e7c
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-05-27 15:34:07 +0100

    Added single-result-box class

--- intermediate_front_end_development_projects/wikipedia_viewer/script.js
+++ intermediate_front_end_development_projects/wikipedia_viewer/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $("#results-list").html("");
     for (var single_result of results) {
       $("#results-list").append(
-        `<li>
+        `<li class='single-result-box'>
           <h3>
             <a class='result-links' href='https://en.wikipedia.org/wiki/${single_result.title}' target='_blank'>
               ${single_result.title}
