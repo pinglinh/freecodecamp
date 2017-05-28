864aec0f884a62e1c38c6c4ef5f017caf004855b
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-05-28 15:30:21 +0100

    Added class for on and off channels

--- intermediate_front_end_development_projects/twitch_tv/script.js
+++ intermediate_front_end_development_projects/twitch_tv/script.js
@@ -3,31 +3,36 @@ function get_user(user, data) {
   console.log(data);
   if (data.stream === null) {
     $(".channels").append(
-      `<p><a href="${link}">${user}</a></p>
-      <p>Offline</p>`
+      `
+      <div class="single-channel-off">
+        <p><a href="${link}">${user}</a></p>
+        <p>Offline</p>
+      </div>
+      `
     );
   }
   else if (data.stream.stream_type === "live") {
     $(".channels").append(
-      `<p><a href="${link}">${user}</a></p>
-       <p>Online</p>
+      `
+      <div class="single-channel-on">
+          <img class="logo" src="${data.stream.channel.logo}">
+           <p><a href="${link}">${user}</a></p>
+           <p>Online</p>
+      </div>
       `
     );
   }
 }
 
-
-
-
-
 $(document).ready(function() {
   var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
   for (let user of users) {
-    console.log(user);
+    console.log(JSON.stringify(user));
     $.getJSON(`https://wind-bow.gomix.me/twitch-api/streams/${user}?callback=?`, function(data) {
         get_user(user, data);
-        console.log("WTF", user)
+        console.log("WTF", user);
+        JSON.stringify(data);
     });
   }
 });
