0a9c3a0f12d4998b942d1396851fdf24ba3e7fcc
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-05-30 01:28:16 +0100

    Removed console logs

--- intermediate_front_end_development_projects/random_quote_machine/json_script.js
+++ intermediate_front_end_development_projects/random_quote_machine/json_script.js
@@ -5,11 +5,8 @@ $(document).ready(function() {
   var authorContainer = $("#author");
   $("#new-quote").on("click", function(){
     $.getJSON( "http://www.yerkee.com/api/fortune", function( data ) {
-    console.log(data);
     var theQuote = data[0].fortune;
     var theAuthor = data[0].title;
-    console.log(theQuote);
-    console.log(theAuthor);
     $(quotesContainer).html(theQuote);
     $(authorContainer).html(theAuthor);
     });
