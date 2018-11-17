8ebfed4f3629a55f4ec019fd48fad0ee75fc9ffd
pinglinh <lnguyenmy1@gmail.com>
2018-11-17 16:25:12 +0000

    Add object literal declarations

---
+++ ES6/write_concise_object_literal_declarations_using_simple_fields.js
@@ -0,0 +1,5 @@
+const createPerson = (name, age, gender) => {
+  "use strict";
+  return { name, age, gender };
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
