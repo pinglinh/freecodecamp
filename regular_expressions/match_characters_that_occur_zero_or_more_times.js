6b034e58728614019f95f5fa7d1f9643e5e8b549
pinglinh <lnguyenmy1@gmail.com>
2018-11-18 02:03:50 +0000

    Update + vs * example

--- regular_expressions/match_characters_that_occur_zero_or_more_times.js
+++ regular_expressions/match_characters_that_occur_zero_or_more_times.js
@@ -1,3 +1,12 @@
 let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
 let chewieRegex = /Aa*/;
 let result = chewieQuote.match(chewieRegex);
+
+// Any letter in a regex expression that is followed by a * does not have to occur in the string tested whereas any letter in a regex expression followed by a + must occur in a string at least once
+let phrase = "ba humbug";
+
+let regexPlus = /bah+/;
+let regexStar = /bah*/;
+
+regexPlus.test(phrase); // returns false
+regexStar.test(phrase); // returns true
