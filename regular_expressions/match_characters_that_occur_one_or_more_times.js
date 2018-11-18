df73d458c9ca7951b1d16deb127acba776834348
pinglinh <lnguyenmy1@gmail.com>
2018-11-18 01:49:51 +0000

    Update exercise with +

--- regular_expressions/match_characters_that_occur_one_or_more_times.js
+++ regular_expressions/match_characters_that_occur_one_or_more_times.js
@@ -1,3 +1,13 @@
 let difficultSpelling = "Mississippi";
 let myRegex = /s+/gi;
 let result = difficultSpelling.match(myRegex);
+
+// This would give [ 'a', 'a', 'a' ]
+let difficultSpelling = "aabab";
+let myRegex = /a/gi;
+let result = difficultSpelling.match(myRegex);
+
+// This would give [ 'aa', 'a' ]
+let difficultSpelling = "aabab";
+let myRegex = /a+/gi;
+let result = difficultSpelling.match(myRegex);
